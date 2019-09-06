426c807888cab4395ac5435d63c12de657b8dbea
JDVelasquezO <jdvela158@gmail.com>
2019-09-06 06:24:21 -0600

    Class Vehicle added

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,12 @@
 package app;
 
-import app.users.*;
+// import app.users.*;
+import app.vehicles.*;
 
 public class App {
     public static void main(String[] args) {
-        User user1 = new Client("JD123", "123", "Daniel", "Velásquez", "454598");
-        System.out.println(user1);
+        // User user = new Client("JD123", "123", "Daniel", "Velásquez", "454598");
+        Vehicle vehicle = new Bus();
+        System.out.println(vehicle);
     }
 }
