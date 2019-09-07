1add38e1dfb38fe963b4101cccf70e1c8ad55d22
Daniel Velásquez <jdvela158@gmail.com>
2019-09-07 14:03:27 +0000

    First use in gitpod

--- src/app/App.java
+++ src/app/App.java
@@ -1,12 +1,12 @@
 package app;
 
-// import app.users.*;
-import app.vehicles.*;
+import app.users.*;
+// import app.vehicles.*;
 
 public class App {
     public static void main(String[] args) {
-        // User user = new Client("JD123", "123", "Daniel", "Velásquez", "454598");
-        Vehicle vehicle = new Bus();
-        System.out.println(vehicle);
+        User user = new Client("JD123", "123", "Daniel", "Velásquez", "454598");
+        // Vehicle vehicle = new Bus();
+        System.out.println(user);
     }
 }
